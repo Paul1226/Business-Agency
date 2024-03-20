0f334c90be280f9c9496560cd19bd28ffc882627
Paul1226 <paulguachichullca023@gmail.com>
2024-03-20 16:57:41 -0500

    se agragó una nueva lista de colores y se aplico a la pág

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,24 +1,38 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			colors: {
-				'primary': '#7EDAD2',
-				'secondary': '#424241',
-				'third': '#909090',
-			},
-			fontFamily: {
-				bebas: ['Bebas Neue', 'cursive'],
-				poppins: ['Poppins', 'sans-serif'],
-			},
-			gridTemplateColumns: {
-				'4s': 'repeat(auto-fill, minmax(250px, 1fr))',
-			},
-			boxShadow: {
-				'boxs': '0 0 10px 6px #90909020',
-			},
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        monteCario: {
+          '50': "#f2fbfa",
+          '100': "#d3f4ef",
+          '200': "#a6e9e1",
+          '300': "#7edad2",
+          '400': "#44bdb6",
+          '500': "#2ba19c",
+          '600': "#20817f",
+          '700': "#1d6867",
+          '800': "#1c5253",
+          '900': "#1b4646",
+          '950': "#0a2829",
+        },
+      },
+      fontFamily: {
+        bebas: ["Bebas Neue", "cursive"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "4s": "repeat(auto-fill, minmax(250px, 1fr))",
+      },
+      boxShadow: {
+        boxs: "0 0 10px 6px #90909020",
+        boxs2: "0 0 10px 6px #2ba19c30",
+      },
+	  backgroundImage:{
+		gradientCard: 'linear-gradient(to top left, #2ba19c 0%, #7edad2 100%, #d3f4ef 100% )',
+	  }
+    },
+  },
+  plugins: [],
+};
